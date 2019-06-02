7d402de3f4cb11f43a1bee91db34eb61f655e010
ivanbisabi <37533058+ivanbisabi@users.noreply.github.com>
2019-06-02 23:59:39 +0200

    00:00
    Ancora c'è da lavorarci però...

---
+++ precache-manifest.347d160c90f0e00d689bc73fed364f45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7302c848102d7cd36c2a8d3ccf7741ed",
+    "url": "/www/comics/index.html"
+  },
+  {
+    "revision": "c10340ba5ad14f0e76d8",
+    "url": "/www/comics/static/css/main.969437c6.chunk.css"
+  },
+  {
+    "revision": "e5bc45b45daf3fa6f7a6",
+    "url": "/www/comics/static/js/2.08e58e18.chunk.js"
+  },
+  {
+    "revision": "c10340ba5ad14f0e76d8",
+    "url": "/www/comics/static/js/main.dab2ae66.chunk.js"
+  },
+  {
+    "revision": "be8dcf9bb3f13cf77629",
+    "url": "/www/comics/static/js/runtime~main.156853a6.js"
+  }
+]);
