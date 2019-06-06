b2609c7af33245b078d05d45cfb0ce7b7ff4fd45
Alessio Cimarelli <jenkin@dataninja.it>
2019-06-06 07:13:09 +0200

    Include build folder

---
+++ build/precache-manifest.83e8cf884109c07cd5e8f5aeab31b1c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73be89c0e8fd32d788f97b585037cb44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e790baefe9c6dee71e7a",
+    "url": "./static/css/main.6c69b0b8.chunk.css"
+  },
+  {
+    "revision": "efa5bb02533d6025dc8b",
+    "url": "./static/js/2.aad5274d.chunk.js"
+  },
+  {
+    "revision": "e790baefe9c6dee71e7a",
+    "url": "./static/js/main.b71635f3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
